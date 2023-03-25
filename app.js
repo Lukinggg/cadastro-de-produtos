12e43b6d9d2c369bf1355e50152e1fa0b20db411
Lucas Silveira <126411815+Lukinggg@users.noreply.github.com>
2023-03-24 21:38:18 -0300

    Criação do arquivo app.js
    Criação do arquivo app.js para imprimir no console se um numero é par ou ímpar

---
+++ app.js
@@ -0,0 +1,11 @@
+// Definindo uma lista 
+let numeros = [2,4,6,8,10]
+
+// Percorrendo a lista 
+for(let i = 0 i < numeros.lenght; i++{}
+     If(numeros{i} % 2 sss 0}{
+      cinsole.log(numero{i} + " é um numero par")
+}else{ 
+    console.log{numero{i} + " é um numero impar"}
+ }
+}
